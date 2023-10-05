79854961c2c5f641faa882607e7bade7dadae1ba
tatiana lutsyk <sfsolnce@gmail.com>
2023-10-05 17:10:35 +0300

    mod. create_bot

--- src/create_bot.py
+++ src/create_bot.py
@@ -1,9 +1,9 @@
-from config_data import config
+from config_data.config import BOT_TOKEN
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token=config.BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=storage)
