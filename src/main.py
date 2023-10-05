3769d99baabb0689d3030ce73d0ff6cb56f5dbd0
tatiana lutsyk <sfsolnce@gmail.com>
2023-10-05 17:08:55 +0300

    mod. main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 from aiogram.utils import executor
 from create_bot import dp
+from handlers import client, lowprice
+from loguru import logger
+
+
+logger.add("debug.log", rotation="10 MB", encoding='utf-8', level='SUCCESS',
+           backtrace=True, diagnose=False, enqueue=True)
+
 
 async def on_startup(_):
     print('Бот онлайн.')
-    
-from handlers import client
+
 
 client.register_handlers_client(dp)
-    
+lowprice.register_handlers_client(dp)
+
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
